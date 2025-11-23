5cc27c9b399e334f0f248ad7d49bf514172db932
Zane Li <zaneli01@qq.com>
2025-11-23 17:52:17 +0800

    更新11.2部分练习代码内容

--- ch10_oop/protected_student.py
+++ ch10_oop/protected_student.py
@@ -1,6 +1,6 @@
 #访问限制
-class Student(object):
-    def __init__(self, name, gender, score):
+class PtStudent(object):
+    def __init__(self, name = 'unkw', gender = 'male', score = '0'):
         self.__name = name
         self.__gender = gender
         self.__score = score
@@ -25,23 +25,26 @@ class Student(object):
         else:
             return 'C'
     
-    def set_score(self, score):
-        if 0 <= score <= 100:
-            self.__score = score
-        else:
+    def set_score(self, score): #可以进行参数检查（11.2小节内容）
+        if not isinstance(score, int):
+            raise ValueError('bad input')
+        elif score < 0 or score > 100:
             raise ValueError('bad score')
+        else:
+            self.__score = score
     
     def set_gender(self, gender):
         self.__gender = gender
 
-zane = Student('Zane Li', 'male', 98)
-# 测试:
-bart = Student('Bart', 'male', 77)
-if bart.get_gender() != 'male':
-    print('测试失败!')
-else:
-    bart.set_gender('female')
-    if bart.get_gender() != 'female':
+if __name__ == "__main__":
+    zane = PtStudent('Zane Li', 'male', 98)
+    # 测试:
+    bart = PtStudent('Bart', 'male', 77)
+    if bart.get_gender() != 'male':
         print('测试失败!')
     else:
-        print('测试成功!')
+        bart.set_gender('female')
+        if bart.get_gender() != 'female':
+            print('测试失败!')
+        else:
+            print('测试成功!')
