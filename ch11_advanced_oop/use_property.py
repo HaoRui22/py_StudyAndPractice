e6570ad97bf95da6efbf805f65ec869dd136eb07
Zane Li <zaneli01@qq.com>
2025-11-24 11:58:56 +0800

    更新11.2部分代码内容

--- ch11_advanced_oop/use_property.py
+++ ch11_advanced_oop/use_property.py
@@ -1,30 +1,11 @@
 import ch10_oop.student as student
-
-# 绑定属性时直接把属性暴露出去，便携简单但无法进行参数检查：
-s = student.Student()
-s.score = 9999
-print(f"分数{s.score}显然不合逻辑")
-
-# 想限制 score 范围，可以通过方法 set_score() get_score() 来设置和获取成绩，在set_score()方法里就可以检查参数：
-# 如同 ../ch10_oop/protected_student.py中定义类的做法
-
 import ch10_oop.protected_student as protected_student
-
-ps = protected_student.PtStudent()
-ps.set_score(100)
-print(ps.get_score())
-# ps.set_score(-1)
-# bad score
-
-# BUT... 在../ch10_oop/protected_student.py中定义类的做法略显复杂
-# 想追求完美，既能检查参数又可以用类似属性这样的简单方式访问类的变量，则需要用到装饰器
-# Python内置的@property装饰器就是负责把一个方法变成属性调用的：
 
 class PpStudent(object):
     def __init__(self, score = 0):
         self.__score = score
 
-    @property
+    @property # 定义 getter 方法，将其包装成属性
     def score(self):
         return self.__score
     
@@ -36,6 +17,28 @@ class PpStudent(object):
             raise ValueError('bad score')
         self.__score = value
 
-pps = PpStudent()
-pps.score = 60
-print(pps.score)
+
+if __name__ == '__main__':
+
+# 绑定属性时直接把属性暴露出去，编写简单但无法进行参数检查：
+    s = student.Student()
+    s.score = 9999
+    print(f"分数{s.score}显然不合逻辑")
+
+# 想限制 score 范围，可以通过方法 set_score() get_score() 来设置和获取成绩，在set_score()方法里就可以检查参数：
+# 如同 ../ch10_oop/protected_student.py中定义类的做法
+    ps = protected_student.PtStudent()
+    ps.set_score(100)
+    print(ps.get_score())
+    # ps.set_score(-1)
+    # ValueError: bad score
+
+# BUT... 在../ch10_oop/protected_student.py中定义类的做法略显复杂
+# 想追求完美，既能检查参数又可以用类似属性这样的简单方式访问类的变量，则需要用到装饰器
+# Python内置的@property装饰器就是负责把一个方法变成属性调用的。请查看类⬆️：PpStudent
+    print("use property⬇️")
+    pps = PpStudent()
+    pps.score = 60
+    print(pps.score)
+    # pps.score = 110
+    # ValueError: bad score
