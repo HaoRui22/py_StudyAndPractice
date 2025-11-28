03faa46015f974c82952619ecfe0ab0ed3422cc9
Zane Li <zaneli01@qq.com>
2025-11-28 18:50:11 +0800

    更新12.3部分内容

---
+++ ch12_error_debug_test/mydict.py
@@ -0,0 +1,15 @@
+# 编写一个Dict类，这个类的行为和dict一致，但是可以通过属性来访问，用起来就像下面这样：
+'''
+>>> d = Dict(a=1, b=2)
+>>> d['a']
+1
+>>> d.a
+1
+'''
+class Dict(dict):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return
